dd0087a0ce26053be052f879d72f5656a0a7cec9
Rajesh Nayak <Rajesh.Nayak@mastercard.com>
2024-06-10 21:56:51 +1000

    episode-02 and 03

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
 // Nested HTML using React Element
 
-const parent = React.createElement(
-    "div", 
-    {id:"parent"}, 
-    [React.createElement("div", {id:"child1"},[
-        React.createElement("h1", {key:1}, "Hello I am H1 tag"),
-        React.createElement("h2", {key:2}, "Hello I am H2 tag")
-    ]),
-    React.createElement("div", {id:"child2"},[
-        React.createElement("h1", {key:3}, "Hello I am H1 tag"),
-        React.createElement("h2", {key:4}, "Hello I am H2 tag")
-    ])
-    ]);
+const heading =(
+    <h1 className="heading">Namaste React</h1>
+);
+
+const HeadingComponent = () => (
+       <div>
+        {heading}
+         <h1>Namaste React</h1>
+       </div>
+    
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent)
+root.render(<HeadingComponent/>)
